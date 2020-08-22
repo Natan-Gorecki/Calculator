29e77c4db72b87efe2304c41af76a73d27e3b8c0
Natan Górecki <97natan@gmail.com>
2020-08-22 18:10:43 +0200

    Defines in api.h file (CC, EXPORT)

--- src/core/api/api.h
+++ src/core/api/api.h
@@ -1,13 +1,42 @@
 #pragma once
 
-//__declspec(dllexport)
-#define DECLSPEC
-
 //Calling Convenion
 //__cdecl v __stdcall
-#define CC
+#define CC __stdcall
 
-//pragma comment ...
+
+// pragma comment and dllimport/dllexport
+#ifdef CALCULATOR
+	#define DSPEC dllexport
+#elif CALCULATOR_STATIC
+	#define DSPEC
+#else
+	#if USE_DLL
+		#define DSPEC dllimport
+		#ifdef _MSC_VER
+			#pragma comment(lib, "Calculator")
+		#else
+			#pragma comment(lib, "Calculator_BCC")
+		#endif
+	#elif USE_STATIC_LIB
+		#define DSPEC
+		#ifdef _MSC_VER
+			#pragma comment(lib, "StaticLibrary")
+		#else
+			#pragma	comment(lib, "StaticLibrary_BCC")
+		#endif
+	#endif 
+#endif
+
+
+// extern "C" __declspec()
+#if defined(CALCULATOR) || defined(USE_DLL)
+	#define EXPORT extern "C" __declspec(DSPEC)
+#else
+	#define EXPORT extern "C"
+#endif
+
+
 
 
 #include "I_Calculator.h"
