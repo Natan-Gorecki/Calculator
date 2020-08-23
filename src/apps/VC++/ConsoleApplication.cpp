ec94c8053a837da18023c8d33ac1901450b0f71f
Natan Górecki <97natan@gmail.com>
2020-08-23 14:58:58 +0200

    Use of export functions and typedefs in console application

--- src/apps/VC++/ConsoleApplication.cpp
+++ src/apps/VC++/ConsoleApplication.cpp
@@ -1,20 +1,69 @@
-// ConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//-----------------------------------------------------------------------------------------------------
+//
+//	USE STATIC LIBRARY "StaticLibrary.lib"
+//	#define USE_STATIC_LIB 1
+//
+//-----------------------------------------------------------------------------------------------------
+//
+//	USE DLL "Calculator.dll" WITH LINKING LIBRARY "Calculator.lib"
+//	#define USE_DLL 1
+//
+//-----------------------------------------------------------------------------------------------------
+//
+//	ifndef USE_DLL || USE_STATIC	=>	DYNAMIC LINK LIBRARY "Calculator.dll"
 //
 
+
+
+#include "api.h"
 #include <iostream>
+#include <libloaderapi.h>
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//-----------------------------------------------------------------------------------------------------
+	//	Get export functions from dll without using linking library
+	//
+	#if !(USE_DLL || USE_STATIC_LIB)
+		HMODULE h_dll = LoadLibrary(L"Calculator");
+
+		if (h_dll == 0) 
+		{
+			std::cout << "Error loading DLL !!!\n";
+			std::cin.get();
+			return 1;
+		}
+
+		create_calculator  CreateCalculator = (create_calculator)GetProcAddress(h_dll, "CreateCalculator");
+		delete_calculator  DeleteCalculator = (delete_calculator)GetProcAddress(h_dll, "DeleteCalculator");
+	#endif
+	//-----------------------------------------------------------------------------------------------------
+	
+
+
+	//******************************************************************************************************
+	I_Calculator* calculator = CreateCalculator();
+	//******************************************************************************************************
+
+
+
+	//******************************************************************************************************
+	DeleteCalculator(calculator);
+	//******************************************************************************************************
+	
+
+
+	//-----------------------------------------------------------------------------------------------------
+	//	Free library from the adress space of calling process
+	//
+	#if !(USE_DLL || USE_STATIC_LIB)
+		if (h_dll != 0)
+			FreeLibrary(h_dll);
+	#endif
+	//-----------------------------------------------------------------------------------------------------
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
