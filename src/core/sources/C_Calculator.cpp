ac1fda5515ac3daffd7a73b7338a5127519b925a
Natan Górecki <97natan@gmail.com>
2020-08-23 12:37:12 +0200

    Added additional methods thah will be needed to implement main function

--- src/core/sources/C_Calculator.cpp
+++ src/core/sources/C_Calculator.cpp
@@ -20,10 +20,157 @@ double CC C_Calculator::Calculate(const char* expression)
 //	Function calculating the result
 //
 //------------------------------------------------------------------------------
-double calculate(const char* text, int start, int end)
+double C_Calculator::calculate(const char* text, int start, int end)
 {
 	// TODO
 	return -1;
+}
+
+
+
+//------------------------------------------------------------------------------
+//	Function check if there is operation outside parentheses to do  
+//	e.g. 2(4*2) - multiply
+//		 2+4	- addition
+//
+//------------------------------------------------------------------------------
+bool C_Calculator::operation_outside_parentheses(const char* tekst, int start, int end) 
+{
+
+	// We're looking for operation ONLY OUTSIDE parentheses
+	// Level must have value 0
+	int level = 0;
+
+
+	//------------------------------------------
+	//	Loop that checks if there is addition or  subtraction
+	//	Return TRUE -	'+' v '-'
+	//------------------------------------------
+	for (int i = end; i >= start; i--) {
+		if (tekst[i] == '+' || tekst[i] == '-')
+			if (level == 0)
+				return true;
+
+		//	Opening the bracket increases level
+		if (tekst[i] == ')')
+			level++;
+
+		//	Closing the bracket decreases level
+		if (tekst[i] == '(')
+			level--;
+	}
+
+
+	//------------------------------------------
+	//	Loop that checks if there is multiplication or division
+	//	Return TRUE -	'*' v '/'
+	//	Return TRUE -	")2" v ")(" v "2("
+	//------------------------------------------
+	for (int i = end; i >= start; i--) {
+		if (tekst[i] == '*' || tekst[i] == '/')
+			if (level == 0)
+				return true;
+
+		//	Opening the bracket increases level
+		if (tekst[i] == ')') {
+			level++;
+
+			//	Return TRUE, when next char is number <==> ") 2"
+			if (i < end && level == 1)
+				if (next_char(tekst, i + 1, end) != -1)
+					if (is_number(tekst[next_char(tekst, i + 1, end)]))
+						return true;
+		}
+
+
+		//	Closing the bracket decreases level
+		if (tekst[i] == '(') {
+			level--;
+
+			//	Return TRUE, when previous char is number or the opposite bracket
+			//	<==> ") (" v "2 ("
+			if (i > start && level == 0)
+				if (previous_char(tekst, start, i - 1) != -1)
+					if (is_number(tekst[previous_char(tekst, start, i - 1)]) || tekst[previous_char(tekst, start, i - 1)] == ')')
+						return true;
+		}
+	}
+
+
+	//	Return false, when no matching operation was found 
+	//	Ramaining number or operations in parentheses
+	return false;
+}
+
+
+
+//------------------------------------------------------------------------------
+//	Function find	LAST	operation to do
+//
+//	Return the place, where find this operation
+//------------------------------------------------------------------------------
+int C_Calculator::finding_last_operation(const char* tekst, int start, int end) 
+{
+
+	// We're looking for operation ONLY OUTSIDE parentheses
+	// Level must have value 0
+	int level = 0;
+
+
+	//------------------------------------------
+	//	Loop that checks if there is addition or  subtraction
+	//	If there is return the place of last '+' v '-'
+	//------------------------------------------
+	for (int i = end; i >= start; i--) {
+		if (tekst[i] == '+' || tekst[i] == '-')
+			if (level == 0)
+				return i;
+
+		//	Opening the bracket increases level
+		if (tekst[i] == ')')
+			level++;
+
+		//	Closing the bracket decreases level
+		if (tekst[i] == '(')
+			level--;
+	}
+
+
+	//------------------------------------------
+	//	Loop that checks if there is multiplication or division
+	//	Return the place of LAST operation to do
+	//------------------------------------------
+	for (int i = end; i >= start; i--) {
+
+		// If there is '*' or '/' return the place of this char
+		if (tekst[i] == '*' || tekst[i] == '/')
+			if (level == 0)
+				return i;
+
+
+		//	Opening the bracket increases level
+		if (tekst[i] == ')') {
+			level++;
+
+			//	Return the place of ')' when multiplication like ") 2" was found
+			if (i < end && level == 1)
+				if (next_char(tekst, i + 1, end) != -1)
+					if (is_number(tekst[previous_char(tekst, i + 1, end)]))
+						return i;
+		}
+
+
+		//	Closing the bracket decreases level
+		if (tekst[i] == '(') {
+			level--;
+
+			//	Return the place of '(' when multiplication like "2 (" or ") (" was found
+			if (i > start && level == 0)
+				if (previous_char(tekst, start, i - 1) != -1)
+					if (is_number(tekst[previous_char(tekst, start, i - 1)]) || tekst[previous_char(tekst, start, i - 1)] == ')')
+						return i;
+		}
+	}
 }
 
 
@@ -34,7 +181,8 @@ double calculate(const char* text, int start, int end)
 //	Return - the place of following char
 //	Return -	-1	in case when there are only whitespace char in searched range
 //-----------------------------------------------------------------
-int C_Calculator::next_char(const char* text, int start, int end) {
+int C_Calculator::next_char(const char* text, int start, int end) 
+{
 	for (int i = start; i <= end; i++)
 		if (text[i] != ' ')
 			return i;
@@ -50,12 +198,96 @@ int C_Calculator::next_char(const char* text, int start, int end) {
 //	Return - the place of previous char
 //	Return -	-1	in case when there are only whitespace char in searched range
 //-----------------------------------------------------------------
-int C_Calculator::previous_char(const char* text, int start, int end) {
+int C_Calculator::previous_char(const char* text, int start, int end) 
+{
 	for (int i = end; i >= start; i--)
 		if (text[i] != ' ')
 			return i;
 
 	return -1;
+}
+
+
+
+//-----------------------------------------------------------------
+//	Return TRUE - '0'-'9' v '.' v ','
+//
+//	Return FALSE - other characters
+//-----------------------------------------------------------------
+bool C_Calculator::is_number(char c) 
+{
+	if (('0' <= c && c <= '9') || c == '.' || c == ',')
+		return true;
+
+	return false;
+}
+
+
+
+//-----------------------------------------------------------------
+//
+//	Funtion check if there are outside parentheses
+//	e.g (2+2)	v	(4*2)
+//	
+//-----------------------------------------------------------------
+bool C_Calculator::are_parentheses(const char* text, int start, int end)
+{
+	for (int i = start; i <= end; i++) {
+		if (text[i] == ' ')
+			continue;
+
+
+		//We find '(' - now we're looking ')' from the other side
+		if (text[i] == '(') {
+			for (int j = end; j > i; j--) {
+				if (text[j] == ' ')
+					continue;
+
+				//We find ')' - return true
+				if (text[j] == ')')
+					return true;
+
+				// Last non-whitespace character isn't rigth bracket - return false
+				return false;
+			}
+		}
+
+
+		// First non-whitespace character isn't left bracket - return false
+		return false;
+	}
+
+
+	//	There are no parentheses - return false;
+	return false;
+}
+
+
+
+//-----------------------------------------------------------------
+//
+//	Function find oustide parentheses and give their location left and rigth variable
+//
+//-----------------------------------------------------------------
+void C_Calculator::finding_parentheses(const char* text, int start, int end, int& left, int& rigth)
+{
+	// The place of left bracket
+	for (int i = start; i <= end; i++)
+		if (text[i] == '(') {
+			left = i;
+			break;
+		}
+
+
+	// The place of rigth bracket
+	for (int i = end; i > left; i--)
+		if (text[i] == ')') {
+			rigth = i;
+			break;
+		}
+
+
+	return;
 }
 
 
--- src/core/sources/C_Calculator.h
+++ src/core/sources/C_Calculator.h
@@ -16,6 +16,21 @@ private:
 
 
 	//-----------------------------------------------------------------
+	//	First function check if there is operation outside parentheses to do  
+	//	e.g. 2(4*2) - multiply
+	//		 2+4	- addition
+	//
+	//	Second function find	LAST	operation to do
+	//
+	//	Return the place, where find this operation
+	//
+	//-----------------------------------------------------------------
+	bool operation_outside_parentheses(const char* tekst, int start, int end);
+	int finding_last_operation(const char* tekst, int start, int end);
+
+
+
+	//-----------------------------------------------------------------
 	//	Functions check next/previous non-whitespace character 
 	//
 	//	Return - the place of following char
@@ -24,6 +39,26 @@ private:
 	//-----------------------------------------------------------------
 	int next_char(const char* text, int start, int end);
 	int previous_char(const char* text, int start, int end);
+
+
+
+	//-----------------------------------------------------------------
+	//	Return TRUE - '0'-'9' v '.' v ','
+	//
+	//	Return FALSE - other characters
+	//-----------------------------------------------------------------
+	bool is_number(char c);
+
+
+
+	//-----------------------------------------------------------------
+	//	First function check if there are outside parentheses
+	//	e.g (2+2)	v	(4*2)
+	//
+	//	Second function find them and give their location left and rigth variable
+	//-----------------------------------------------------------------
+	bool are_parentheses(const char* text, int start, int end);
+	void finding_parentheses(const char* text, int start, int end, int& left, int& rigth);
 
 
 
