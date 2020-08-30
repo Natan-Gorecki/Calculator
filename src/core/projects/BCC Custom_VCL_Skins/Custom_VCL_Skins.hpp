9516f684ebba2e7127c0b9c662c00bfd6cf029e5
Natan Górecki <97natan@gmail.com>
2020-08-30 18:49:16 +0200

    Addded CPP project and missing file Custom_VCL_Skins.hpp

---
+++ src/core/projects/BCC Custom_VCL_Skins/Custom_VCL_Skins.hpp
@@ -0,0 +1,57 @@
+﻿// CodeGear C++Builder
+// Copyright (c) 1995, 2017 by Embarcadero Technologies, Inc.
+// All rights reserved
+
+// (DO NOT EDIT: machine generated header) 'Custom_VCL_Skins.pas' rev: 33.00 (Windows)
+
+#ifndef Custom_vcl_skinsHPP
+#define Custom_vcl_skinsHPP
+
+#pragma delphiheader begin
+#pragma option push
+#pragma option -w-      // All warnings off
+#pragma option -Vx      // Zero-length empty class member 
+#pragma pack(push,8)
+#include <System.hpp>
+#include <SysInit.hpp>
+#include <System.Classes.hpp>
+#include <dxCore.hpp>
+#include <dxCoreGraphics.hpp>
+#include <dxGDIPlusAPI.hpp>
+#include <cxLookAndFeelPainters.hpp>
+#include <dxSkinsCore.hpp>
+#include <dxSkinsLookAndFeelPainter.hpp>
+
+//-- user supplied -----------------------------------------------------------
+
+namespace Custom_vcl_skins
+{
+//-- forward type declarations -----------------------------------------------
+class DELPHICLASS TdxSkinDefaultPainter;
+//-- type declarations -------------------------------------------------------
+#pragma pack(push,4)
+class PASCALIMPLEMENTATION TdxSkinDefaultPainter : public Dxskinslookandfeelpainter::TdxSkinLookAndFeelPainter
+{
+	typedef Dxskinslookandfeelpainter::TdxSkinLookAndFeelPainter inherited;
+	
+public:
+	virtual System::UnicodeString __fastcall LookAndFeelName();
+public:
+	/* TdxSkinLookAndFeelPainter.Create */ inline __fastcall virtual TdxSkinDefaultPainter(const System::UnicodeString ASkinResName, NativeUInt ASkinResInstance) : Dxskinslookandfeelpainter::TdxSkinLookAndFeelPainter(ASkinResName, ASkinResInstance) { }
+	/* TdxSkinLookAndFeelPainter.Destroy */ inline __fastcall virtual ~TdxSkinDefaultPainter() { }
+	
+};
+
+#pragma pack(pop)
+
+//-- var, const, procedure ---------------------------------------------------
+}	/* namespace Custom_vcl_skins */
+#if !defined(DELPHIHEADER_NO_IMPLICIT_NAMESPACE_USE) && !defined(NO_USING_NAMESPACE_CUSTOM_VCL_SKINS)
+using namespace Custom_vcl_skins;
+#endif
+#pragma pack(pop)
+#pragma option pop
+
+#pragma delphiheader end.
+//-- end unit ----------------------------------------------------------------
+#endif	// Custom_vcl_skinsHPP
