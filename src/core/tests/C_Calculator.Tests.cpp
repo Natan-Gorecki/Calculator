fc024542e0ca0cbd317e8837a046c5eb533e6b71
Natan Górecki <97natan@gmail.com>
2020-10-18 20:11:37 +0200

    Added MS Tests project - started introduce unit tests to projects

---
+++ src/core/tests/C_Calculator.Tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CalculatorTests
+{
+	TEST_CLASS(CalculatorTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
