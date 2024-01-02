5be058860c7113344b404629922e000bbdcbd7ce
Natan Górecki <97natan@gmail.com>
2024-01-02 05:31:16 +0100

    Adjusted post-build scripts

--- src/core/projects/VS DLL/post-build-x64-debug.bat
+++ src/core/projects/VS DLL/post-build-x64-debug.bat
@@ -1,11 +1,5 @@
-:: Generate embarcadero linking library
-mkexp Calculator_BCC.a "..\..\..\..\out\bin\x64\Debug\Calculator.dll"
-
-
 :: Create directory if doesnt exist
 mkdir "..\..\..\..\out\lib\x64\Debug\"
 
-
-:: Move linking libraries to lib directory
+:: Move linking library to lib directory
 move "..\..\..\..\out\bin\x64\Debug\Calculator.lib" "..\..\..\..\out\lib\x64\Debug\"
-move Calculator_BCC.lib "..\..\..\..\out\lib\x64\Debug\"
--- src/core/projects/VS DLL/post-build-x86-debug.bat
+++ src/core/projects/VS DLL/post-build-x86-debug.bat
@@ -1,11 +1,5 @@
-:: Generate embarcadero linking library
-implib -a Calculator_BCC.lib "..\..\..\..\out\bin\x86\Debug\Calculator.dll"
-
-
 :: Create directory if doesnt exist
 mkdir "..\..\..\..\out\lib\x86\Debug\"
 
-
-:: Move linking libraries to lib directory
+:: Move linking library to lib directory
 move "..\..\..\..\out\bin\x86\Debug\Calculator.lib" "..\..\..\..\out\lib\x86\Debug\"
-move Calculator_BCC.lib "..\..\..\..\out\lib\x86\Debug\"
