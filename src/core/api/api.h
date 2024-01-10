d29c0dac284d130a8bae9ff58817b71948770c13
Natan Górecki <97natan@gmail.com>
2024-01-10 07:23:04 +0100

    Converted tabs to spaces

--- src/core/api/api.h
+++ src/core/api/api.h
@@ -1,48 +1,39 @@
 #pragma once
 
-
-#ifndef NULL
-	#define NULL 0
-#endif
-
-
 //Calling Convenion
 //__cdecl v __stdcall
 #ifndef CC
-	#define CC __cdecl
-#endif 
-
-
+    #define CC __cdecl
+#endif
 
 // pragma comment and dllimport/dllexport
 #ifdef CALCULATOR
-	#define DSPEC dllexport
+    #define DSPEC dllexport
 #elif CALCULATOR_STATIC
-	#define DSPEC
+    #define DSPEC
 #else
-	#if USE_DLL
-		#define DSPEC dllimport
-		#ifdef _MSC_VER
-			#pragma comment(lib, "Calculator")
-		#else
-			#pragma comment(lib, "Calculator_BCC")
-		#endif
-	#elif USE_STATIC_LIB
-		#define DSPEC
-		#ifdef _MSC_VER
-			#pragma comment(lib, "StaticLibrary")
-		#else
-			#pragma	comment(lib, "StaticLibrary_BCC")
-		#endif
-	#endif 
+    #if USE_DLL
+        #define DSPEC dllimport
+        #ifdef _MSC_VER
+            #pragma comment(lib, "CalculatorDynamic")
+        #else
+            #pragma comment(lib, "CalculatorDynamic_BCC")
+        #endif
+    #elif USE_STATIC_LIB
+        #define DSPEC
+        #ifdef _MSC_VER
+            #pragma comment(lib, "CalculatorStatic")
+        #else
+            #pragma comment(lib, "CalculatorStatic_BCC")
+        #endif
+    #endif 
 #endif
-
 
 // extern "C" __declspec()
 #if defined(CALCULATOR) || defined(USE_DLL)
-	#define EXPORT extern "C" __declspec(DSPEC)
+    #define EXPORT extern "C" __declspec(DSPEC)
 #else
-	#define EXPORT extern "C"
+    #define EXPORT extern "C"
 #endif
 
 
@@ -51,7 +42,7 @@
 
 
 //-----------------------------------------------------------------------------------------------------
-//	Export functions
+//  Export functions
 //
 EXPORT ICalculator* CC CreateCalculator();
 EXPORT void CC DeleteCalculator(ICalculator* calculator);
@@ -60,7 +51,7 @@ EXPORT void CC DeleteCalculator(ICalculator* calculator);
 
 
 //-----------------------------------------------------------------------------------------------------
-//	Typedefs
+//  Typedefs
 //
 typedef ICalculator* (CC* create_calculator)();
 typedef void (CC* delete_calculator)(ICalculator*);
@@ -69,7 +60,7 @@ typedef void (CC* delete_calculator)(ICalculator*);
 
 
 //-----------------------------------------------------------------------------------------------------
-//	Error Callback
+//  Error Callback
 //
 typedef void (CC* ErrorCallback)(const char* message);
 
