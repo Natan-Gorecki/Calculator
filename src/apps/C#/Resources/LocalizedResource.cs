749ca2269b6ad75e55f5841bc6f23eaec45cc84d
Natan Górecki <97natan@gmail.com>
2024-01-27 16:19:00 +0100

    Prepared localized resources that allow to change language at runtime

---
+++ src/apps/C#/Resources/LocalizedResource.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Data;
+
+namespace CalculatorWPF.Resources;
+
+public class LocalizedResource : Binding
+{
+    public LocalizedResource(string name) : base($"[{name}]")
+    {
+        Mode = BindingMode.OneWay;
+        Source = LocalizedResources.Instance;
+    }
+}
--- src/apps/C#/Services/LanguageService.cs
+++ src/apps/C#/Services/LanguageService.cs
@@ -1,5 +1,7 @@
 ﻿using CalculatorWPF.Models;
+using CalculatorWPF.Resources;
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Globalization;
 
 namespace CalculatorWPF.Services;
 
@@ -15,15 +17,20 @@ public class LanguageService : ObservableObject, ILanguageService
             {
                 OnPropertyChanged(nameof(IsEnglish));
                 OnPropertyChanged(nameof(IsPolish));
-                UpdateLanguage();
+                LocalizedResources.Instance.CurrentCulture = GetCultureInfo();
             }
         }
     }
     public bool IsEnglish => CurrentLanguage == ELanguage.English;
     public bool IsPolish => CurrentLanguage == ELanguage.Polish;
 
-    private void UpdateLanguage()
+    private CultureInfo GetCultureInfo()
     {
-
+        var cultureName = _currentLanguage switch
+        {
+            ELanguage.Polish => "pl",
+            _ => "en"
+        };
+        return new CultureInfo(cultureName);
     }
 }
