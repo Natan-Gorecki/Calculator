e1eaf35b2fd9e3601121d26af000ca43b43849fd
Natan Górecki <97natan@gmail.com>
2024-02-01 00:10:54 +0100

    Separated clear buttons with another view and viewmodel

---
+++ src/apps/C#/ViewModels/ClearButtonsViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CalculatorWPF.Services;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace CalculatorWPF.ViewModels;
+
+public class ClearButtonsViewModel
+{
+    public ICalculatorService CalculatorService { get; } = App.Ioc.GetRequiredService<ICalculatorService>();
+    public ILayoutService LayoutService { get; } = App.Ioc.GetRequiredService<ILayoutService>();
+
+    public ICommand ClearEntry { get; }
+    public ICommand ClearAll { get; }
+    public ICommand ClearLast { get; }
+
+    public ClearButtonsViewModel()
+    {
+        ClearEntry = new RelayCommand(OnClearEntry);
+        ClearAll = new RelayCommand(OnClearAll);
+        ClearLast = new RelayCommand(OnClearLast);
+    }
+
+    private void OnClearEntry()
+    {
+        CalculatorService.Expression = "";
+    }
+
+    private void OnClearAll()
+    {
+        CalculatorService.Summary = "";
+        CalculatorService.Expression = "";
+        CalculatorService.CalculationEntries.Clear();
+    }
+
+    private void OnClearLast()
+    {
+        if (CalculatorService.Expression.Length > 0)
+        {
+            CalculatorService.Expression = CalculatorService.Expression.Remove(CalculatorService.Expression.Length - 1);
+        }
+    }
+}
